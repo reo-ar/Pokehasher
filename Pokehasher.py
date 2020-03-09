5c2e0bb3006f6057cf03a5f513af786ab714efcf
Yui <yui_cosp@yahoo.co.jp>
2020-03-10 00:10:41 +0900

    Fixed hashes being overwritten, made form handling better

--- Pokehasher.py
+++ Pokehasher.py
@@ -13,7 +13,7 @@ async def fetch():
 
     loop = True
     name = 'https://www.pokemon.com/us/pokedex/'
-    id = 0
+    id = 385
     pklist = {}
 
     while loop == True:
@@ -26,11 +26,15 @@ async def fetch():
                 else:
                     print('No more Pokemon found, stopping.')
                     return
+
         soup = BeautifulSoup(text, 'html.parser')
         imgb = soup.find("div", {'class':'profile-images'})
         inat = imgb.find_all('img')
+
         for img in inat:
+
             pknm = img.get('alt')
+
             if 'Alola' in pknm:
                 ik = soup.title.string
                 pkn = ik.split(' ', 1)[0]
@@ -41,22 +45,26 @@ async def fetch():
                 pknm = 'Galarian %s' %pkn
             elif 'Form' in pknm:
                 ik = soup.title.string
-                pknm = ik.split(' ', 1)[0]
+                pkn = ik.split(' ', 1)[0]
+                pknm = '%s %s' %(pknm, pkn)
             pklink = img.get('src')
             img = BytesIO()
+
             async with ClientSession() as session:
                 async with session.get(pklink) as res:
                     if res.status==200:
                         img.write(await res.read())
                     else:
                         raise Exception('Image didn\'t return 200')
+
             imgh = Image.open(img)
             dhash = imagehash.dhash(imgh)
             hd = str(dhash)
-            pklist[pknm] = hd
+            if 'pknm' not in pklist.keys():
+                pklist[pknm] = hd
             print('Pokemon: %s , Hash: %s' %(pknm, hd))
 
-    with open('dhash.json', 'w') as o:
-        json.dump(pklist, o)
+        with open('dhash.json', 'w') as o:
+            json.dump(pklist, o)
 
 asyncio.get_event_loop().run_until_complete(fetch())
